65011a3801d6e644e40603c6624db62598f24a4d
Miguel Pagola <mipagola@gmail.com>
2023-02-23 16:25:22 +0100

    refactor: extract to class and load as module

--- js/drop-photos.js
+++ js/drop-photos.js
@@ -1,70 +1,74 @@
-function dropHandler(ev) {
-    ev.preventDefault();
+class DropFiles {
 
-    const files = _collectFiles(ev);
-    const images = _collectImages(files);
+    addImages = images => {
+        const container = document.getElementById('images-dragged');
+        const template = document.querySelector('#photo-item');
 
-    _addImages(images);
-}
+        images.forEach(image => {
+            const clone = template.content.cloneNode(true);
 
-function dragOverHandler(ev) {
-    ev.preventDefault();
-}
+            const img = clone.querySelector('img');
+            img.src = URL.createObjectURL(image);
+            img.alt = image.name;
 
-_addImages = images => {
-    const container = document.getElementById('images-dragged');
-    const template = document.querySelector('#photo-item');
+            const exif = clone.querySelector('code');
+            this._extractExif(image)
+                .then((exifData) => {
+                    const summary = {
+                        Camera: exifData.Model.description,
+                        Date: exifData.DateTime.description,
+                        Location: `${exifData.GPSLatitude.description} ${exifData.GPSLatitudeRef.value[0]}, ${exifData.GPSLongitude.description} ${exifData.GPSLongitudeRef.value[0]}`
+                    }
+                    exif.innerHTML = [
+                        hljs
+                            .highlight(JSON.stringify(summary, null, 2), { language: 'json' })
+                            .value,
+                        "Full Metadata",
+                        hljs
+                            .highlight(JSON.stringify(exifData, null, 2), { language: 'json' })
+                            .value
+                    ].join('\n');
+                })
+                .catch((error) => console.log(error));
 
-    images.forEach(image => {
-        const clone = template.content.cloneNode(true);
-
-        const img = clone.querySelector('img');
-        img.src = URL.createObjectURL(image);
-        img.alt = image.name;
-
-        const exif = clone.querySelector('code');
-        _extractExif(image)
-            .then((exifData) => {
-                const summary = {
-                    Camera: exifData.Model.description,
-                    Date: exifData.DateTime.description,
-                    Location: `${exifData.GPSLatitude.description} ${exifData.GPSLatitudeRef.value[0]}, ${exifData.GPSLongitude.description} ${exifData.GPSLongitudeRef.value[0]}`
-                }
-                exif.innerHTML = [
-                    hljs
-                    .highlight(JSON.stringify(summary, null, 2), { language: 'json' })
-                    .value,
-                "Full Metadata",
-                hljs
-                    .highlight(JSON.stringify(exifData, null, 2), { language: 'json' })
-                    .value
-                ].join('\n');
-            })
-            .catch((error) => console.log(error));
-
-        container.appendChild(clone);
-    });
-}
-
-_collectFiles = event => {
-    const { items } = event.dataTransfer;
-
-    let files;
-    if (items) {
-        files = [...items]
-            .filter(item => item.kind === 'file')
-            .map(image => image.getAsFile())
-    }
-    else {
-        files = [...event.dataTransfer.files];
+            container.appendChild(clone);
+        });
     }
 
-    return files;
+    collectFiles = event => {
+        const { items } = event.dataTransfer;
+
+        let files;
+        if (items) {
+            files = [...items]
+                .filter(item => item.kind === 'file')
+                .map(image => image.getAsFile())
+        }
+        else {
+            files = [...event.dataTransfer.files];
+        }
+
+        return files;
+    }
+
+    collectImages = files => files.filter(file => file.type.startsWith('image/'));
+
+    async _extractExif(file) {
+        let exif = await ExifReader.load(file);
+        return exif;
+    }
 }
 
-_collectImages = files => files.filter(file => file.type.startsWith('image/'));
+window.dropHandler = function (ev) {
+    ev.preventDefault();
 
-const _extractExif = async function (file) {
-    let exif = await ExifReader.load(file);
-    return exif;
+    const dropFiles = new DropFiles();
+    const files = dropFiles.collectFiles(ev);
+    const images = dropFiles.collectImages(files);
+
+    dropFiles.addImages(images);
+}
+
+window.dragOverHandler = function (ev) {
+    ev.preventDefault();
 }
