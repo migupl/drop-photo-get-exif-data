adabaadfab2b48f681ef52ad1e08e9f70589ade2
Miguel Pagola <mipagola@gmail.com>
2023-02-24 13:15:34 +0100

    feat: load style and image

--- components/drop-photo-for-exif-component/drop-photo-for-exif.js
+++ components/drop-photo-for-exif-component/drop-photo-for-exif.js
@@ -1,7 +1,26 @@
+import { svgCss } from "./drop-photo-for-exi-dom.js";
+
 class DropPhotoForExif extends HTMLElement {
 
     constructor() {
         super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        const css = document.createElement('style');
+        css.innerHTML = svgCss;
+        this.shadowRoot.appendChild(css);
+
+        const div = document.createElement('div');
+        div.className = 'svg-container';
+        this.shadowRoot.appendChild(div);
+
+        const svg = document.createElement('object');
+        svg.setAttribute('class', 'svg-object');
+        svg.setAttribute('type', 'image/svg+xml');
+        svg.setAttribute('data', './components/drop-photo-for-exif-component/drop-photo.svg');
+        div.appendChild(svg);
     }
 }
 
