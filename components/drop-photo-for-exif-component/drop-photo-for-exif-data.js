f063c57fed89c6bdddb417027a768ffc2e8a26d8
Miguel Pagola <mipagola@gmail.com>
2023-02-27 19:00:51 +0100

    feat: location info is optional

--- components/drop-photo-for-exif-component/drop-photo-for-exif-data.js
+++ components/drop-photo-for-exif-component/drop-photo-for-exif-data.js
@@ -2,13 +2,18 @@ class DropPhotoForExifData {
 
     async extractExif(file) {
         let exif = await ExifReader.load(file);
-        return {
-            location: {
-                latitude: `${exif.GPSLatitude.description} ${exif.GPSLatitudeRef.value[0]}`,
-                longitude: `${exif.GPSLongitude.description} ${exif.GPSLongitudeRef.value[0]}`
-            },
+        let data = {
             details: exif
         };
+
+        if (exif.GPSLatitude && exif.GPSLongitude) {
+            data.location = {
+                latitude: `${exif.GPSLatitude.description} ${exif.GPSLatitudeRef.value[0]}`,
+                longitude: `${exif.GPSLongitude.description} ${exif.GPSLongitudeRef.value[0]}`
+            }
+        }
+
+        return data;
     }
 }
 
