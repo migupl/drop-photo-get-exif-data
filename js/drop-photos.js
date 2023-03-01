2f634a2d3e1a711300cb7d571bf8b6fca7c00461
Miguel Pagola <mipagola@gmail.com>
2023-03-01 11:30:43 +0100

    feat: web component fire and event with image and exif data

--- js/drop-photos.js
+++ js/drop-photos.js
@@ -44,3 +44,25 @@ window.dropHandler = function (ev) {
 window.dragOverHandler = function (ev) {
     ev.preventDefault();
 }
+
+document.addEventListener("drop-photo-for-exif:data", (event) => {
+    event.preventDefault();
+
+    const template = document.getElementById('photo-item');
+    const container = document.getElementById('images-dragged');
+
+    const data = event.detail;
+    const clone = template.content.cloneNode(true);
+
+    const img = clone.querySelector('img');
+    img.src = URL.createObjectURL(data.image);
+    img.alt = data.name;
+
+    const location = clone.getElementById('location');
+    location.innerHTML = highlight(data.location);
+
+    const details = clone.getElementById('details');
+    details.innerHTML = highlight(data.exif);
+
+    container.appendChild(clone);
+});
