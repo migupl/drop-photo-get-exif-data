e0404a2427551498a7ad464b1100675b6605f367
Miguel Pagola <mipagola@gmail.com>
2023-03-01 12:18:58 +0100

    refactor: remove all code for the initial guide

--- js/drop-photos.js
+++ js/drop-photos.js
@@ -1,6 +1,3 @@
-import { dropFiles } from "../components/drop-photo-for-exif-component/drop-photo-for-exif-files.js";
-import { exifData } from "../components/drop-photo-for-exif-component/drop-photo-for-exif-data.js";
-
 function highlight(json, language = 'json') {
     let html = '';
     if (json) {
@@ -11,38 +8,6 @@ function highlight(json, language = 'json') {
     }
 
     return html;
-}
-
-window.dropHandler = function (ev) {
-    ev.preventDefault();
-
-    const template = document.getElementById('photo-item');
-    const container = document.getElementById('images-dragged');
-
-    const images = dropFiles.collectImages(ev);
-
-    images.forEach((image) => {
-        const clone = template.content.cloneNode(true);
-
-        const img = clone.querySelector('img');
-        img.src = URL.createObjectURL(image);
-        img.alt = image.name;
-
-        const location = clone.getElementById('location');
-        const details = clone.getElementById('details');
-
-        exifData.extractExif(image)
-        .then((exif) => {
-            location.innerHTML = highlight(exif.location);
-            details.innerHTML = highlight(exif.details);
-        });
-
-        container.appendChild(clone);
-    });
-}
-
-window.dragOverHandler = function (ev) {
-    ev.preventDefault();
 }
 
 document.addEventListener("drop-photo-for-exif:data", (event) => {
