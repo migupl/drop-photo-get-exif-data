864c85a468e37f911fb911e4f0f24c0f678f8fea
Miguel Pagola <mipagola@gmail.com>
2023-04-21 13:17:09 +0200

    refactor: rename parameter

--- components/drop-photo-for-exif-component/drop-photo-for-exif.js
+++ components/drop-photo-for-exif-component/drop-photo-for-exif.js
@@ -61,11 +61,11 @@ class DropPhotoForExif extends HTMLElement {
         files.directories.forEach(this.#fireDirectory);
     })
 
-    #fireDirectory = directories => {
+    #fireDirectory = directory => {
         const evt = new CustomEvent('drop-photo-for-exif:directory', {
             bubbles: true,
             composed: true,
-            detail: directories
+            detail: directory
         });
         this.shadowRoot.dispatchEvent(evt);
     }
