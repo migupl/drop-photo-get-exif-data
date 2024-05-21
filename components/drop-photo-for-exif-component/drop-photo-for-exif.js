fdb5631906c357a178526108d626d772c8a75028
Miguel Pagola <mipagola@gmail.com>
2024-05-21 17:13:01 +0200

    refactor: declare CSS inside the self-executing anonymous function

--- components/drop-photo-for-exif-component/drop-photo-for-exif.js
+++ components/drop-photo-for-exif-component/drop-photo-for-exif.js
@@ -1,5 +1,4 @@
 import { dropFiles } from "./drop-photo-for-exif-files.js";
-import { shadowCss } from "./drop-photo-for-exif-dom.js";
 
 ; ((exifReaderLib) => {
     class DropPhotoForExif extends HTMLElement {
@@ -141,6 +140,36 @@ import { shadowCss } from "./drop-photo-for-exif-dom.js";
             });
         }
     }
+
+    const shadowCss = `
+:host {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 10px;
+  border-style: dashed;
+
+  position: absolute;
+  left: 50%;
+  -ms-transform: translate(-50%);
+  transform: translate(-50%);
+}
+
+.item {
+  margin: 10px;
+}
+
+div object {
+  min-height: 50px;
+}
+
+div > * {
+  position: relative;
+  z-index: -1;
+}
+`
 
     let exifReaderScriptEl = document.createElement('script');
     exifReaderScriptEl.src = exifReaderLib;
