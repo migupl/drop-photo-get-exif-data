99dceefe8dd86bb424c62c06a1b0539ba57333f6
Miguel Pagola <mipagola@gmail.com>
2024-05-31 16:12:05 +0200

    refactor: change template string to single quotes

--- components/drop-photo-for-exif-component/drop-photo-for-exif.js
+++ components/drop-photo-for-exif-component/drop-photo-for-exif.js
@@ -133,35 +133,31 @@ import { dropFiles } from "./drop-photo-for-exif-files.js";
 
             this.#config = isMobile ? chooseFiles : dropFiles
             this.#config.isMobile = isMobile
-            this.#config.style = `
-:host {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-
-    border-radius: 10px;
-    border-style: dashed;
-
-    position: absolute;
-    left: 50%;
-    -ms-transform: translate(-50%);
-    transform: translate(-50%);
-}
-
-.item {
-    margin: 10px;
-}
-
-div svg {
-    min-height: 50px;
-}
-
-div > * {
-    position: relative;
-    z-index: -1;
-}
-`
+            this.#config.style =
+                ':host {' +
+                    'display: flex;' +
+                    'flex-wrap: wrap;' +
+                    'justify-content: center;' +
+                    'align-items: center;' +
+                    '' +
+                    'border-radius: 10px;' +
+                    'border-style: dashed;' +
+                    '' +
+                    'position: absolute;' +
+                    'left: 50%;' +
+                    '-ms-transform: translate(-50%);' +
+                    'transform: translate(-50%);' +
+                '}' +
+                '.item {' +
+                    'margin: 10px;' +
+                '}' +
+                'div svg {' +
+                    'min-height: 50px;' +
+                '}' +
+                'div > * {' +
+                    'position: relative;' +
+                    'z-index: -1;' +
+                '}'
         }
 
         #stopDefaultsForDragAndDropEvents = () => {
