bfb33fe6c1f261d79d96671237b49a7853f203b1
Miguel Pagola <mipagola@gmail.com>
2024-06-06 17:12:25 +0200

    refactor: include code from removed file

--- components/drop-photo-for-exif-component/drop-photo-for-exif.js
+++ components/drop-photo-for-exif-component/drop-photo-for-exif.js
@@ -1,5 +1,3 @@
-import { dropFiles } from "./drop-photo-for-exif-files.js";
-
 ; ((exifReaderLib) => {
     class DropPhotoForExif extends HTMLElement {
 
@@ -145,6 +143,101 @@ import { dropFiles } from "./drop-photo-for-exif-files.js";
             });
         }
     }
+
+    const dropFiles = function (afterImageReady = (image, exif) => console.log('Do something after image is ready')
+        , afterFileReady = file => console.log('Do something after file is ready')
+        , afterCompletion = () => console.log('Do something on complete')) {
+
+        const significantMimetypes = {
+            geojson: 'application/geo+json',
+            jfif: 'image/jpeg',
+            jpeg: 'image/jpeg',
+            jpg: 'image/jpeg',
+            pjpeg: 'image/jpeg',
+            pjp: 'image/jpeg',
+            png: 'image/png',
+            tiff: 'image/tiff',
+            tif: 'image/tiff',
+            webp: 'image/webp'
+        };
+
+        const supportsWebkitGetAsEntry = 'webkitGetAsEntry' in DataTransferItem.prototype;
+        let unproccessedItems = 0;
+
+        const exploreDirectoryContent = directory => {
+            directory
+                .createReader()
+                .readEntries(processDirectoryContent);
+        }
+
+        const getExifMetadata = file => ExifReader
+            .load(file)
+            .then(exif => {
+                let data = {
+                    details: exif
+                };
+
+                if (exif.GPSLatitude && exif.GPSLatitudeRef && exif.GPSLongitude && exif.GPSLongitudeRef) {
+                    data.location = {
+                        latitude: `${exif.GPSLatitude.description} ${exif.GPSLatitudeRef.value[0]}`,
+                        longitude: `${exif.GPSLongitude.description} ${exif.GPSLongitudeRef.value[0]}`
+                    }
+
+                    if (exif.GPSAltitude) {
+                        const [value, divisor] = exif.GPSAltitude.value;
+                        data.location.altitude = value / divisor;
+                    }
+                }
+
+                return data
+            })
+
+        const getMimetype = filename => {
+            const ext = filename.split('.').pop();
+            return significantMimetypes[ext] || ''
+        }
+
+        const onFileReady = (file, exif) => {
+            exif ? afterImageReady(file, exif) : afterFileReady(file);
+
+            --unproccessedItems;
+            if (!unproccessedItems) afterCompletion();
+        };
+
+        const processDirectoryContent = entries => {
+            const files = entries.filter(entry => entry.isFile)
+
+            files.forEach(entryFile =>
+                entryFile.file(processFile)
+            )
+        }
+
+        const processFile = async file => {
+            ++unproccessedItems
+
+            const typedFile = file.type ? file : new File([file], file.name, { type: getMimetype(file.name) })
+            const exifMetadata = typedFile.type.startsWith('image/') && await getExifMetadata(typedFile);
+
+            onFileReady(typedFile, exifMetadata)
+        }
+
+        const process = items => {
+            for (let item of items) {
+                if (item.name) {
+                    processFile(item)
+                } else if (supportsWebkitGetAsEntry && item.webkitGetAsEntry().isDirectory) {
+                    exploreDirectoryContent(item.webkitGetAsEntry())
+                } else {
+                    processFile(item.getAsFile())
+                }
+            }
+
+        }
+
+        return {
+            process
+        }
+    };
 
     let exifReaderScriptEl = document.createElement('script');
     exifReaderScriptEl.src = exifReaderLib;
